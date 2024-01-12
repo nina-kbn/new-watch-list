eb0428de6a4eab529c65be882ce9096afb5afcee
Nina Laouadi <nina.laouadi0@gmail.com>
2024-01-12 23:07:33 +0100

    tout ok sauf edit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,7 +8,7 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
     @booking = Booking.new
-    @disabled_dates = @car.bookings.flat_map { |booking| (booking.begin_date.to_date..booking.end_date.to_date).map { |date| date.strftime('%Y-%m-%d')
+    @disabled_dates = @car.bookings.flat_map { |booking| (booking.begin_date.to_date..booking.end_date.to_date).map { |date| date.strftime('%d-%m-%Y')
       }}
   end
 end
